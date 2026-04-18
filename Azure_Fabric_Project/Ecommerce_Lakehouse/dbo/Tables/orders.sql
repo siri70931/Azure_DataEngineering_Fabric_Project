b7f0463b869b3843531a244e44942952b2cdec5e
Sireesha <104989646+siri70931@users.noreply.github.com>
2026-04-18 13:24:03 +0530

    Ecommerce Lake house

---
+++ Azure_Fabric_Project/Ecommerce_Lakehouse/dbo/Tables/orders.sql
@@ -0,0 +1,8 @@
+CREATE TABLE [dbo].[orders] (
+
+	[order_id] varchar(8000) NULL, 
+	[customer_id] varchar(8000) NULL, 
+	[order_date] varchar(8000) NULL, 
+	[amount] varchar(8000) NULL, 
+	[status] varchar(8000) NULL
+);
